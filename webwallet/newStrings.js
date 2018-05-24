6864cc7b48092bd8a5a8ea55f746ac514be57eb4
Cristina Guncay <mguncay@ioet.com>
2018-05-24 15:05:28 -0500

    add web wallet

---
+++ webwallet/newStrings.js
@@ -0,0 +1,18 @@
+  //CURRENCY (defaults-english)
+    webString_currencySymbol = "$";
+    webString_questionMark = "?";
+    webString_parensOpen = "(";
+    webString_parensClose = ")";
+
+  //SELF
+    webString_me = "Me";
+    webString_for = "for";
+    webString_quantity = "Qty";
+    webString_justBought = "You have just bought %@ strings";
+
+  //GENERAL NOTIFICTIONS AND ALERTS
+    webString_doneString = "done";
+    webString_confirmButtonString = "OK";
+    webString_cancelButtonString = "Cancel";
+    webString_successAlertTitle = "Success!";
+    
